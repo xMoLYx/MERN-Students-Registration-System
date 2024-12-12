6560392a63739c3121cfb148811ca13d0de4c868
Kacper Milnikel <88721178+xMoLYx@users.noreply.github.com>
2024-12-12 23:57:27 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const {mongoose} = require('mongoose');
-const cookieParser = require('cookie-parser')
+const { mongoose } = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-//db connection
+// db connection
 mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Database connected'))
-.catch((err) => console.log('Database not connected'))
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error('Database connection error:', err));
 
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+// CORS configuration
 const corsOptions = {
   origin: [
     'http://localhost:4173', // Lokalny adres dla testów
     'http://localhost:5173', // Lokalny adres dla testów (Vite)
     'https://mern-students-registration-system-kli3-nyki9h6or.vercel.app', // Frontend na Vercel
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Dozwolone metody
-  credentials: true, // Umożliwia ciasteczka i nagłówki uwierzytelniania
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 };
-
-//middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({extended: false}))
-app.use(cors());
-app.use('/', require('./routes/authRoutes'))
 app.use(cors(corsOptions));
 
-const port = 2525;
+// routes
+app.use('/', require('./routes/authRoutes'));
+
+// dynamic port
+const port = process.env.PORT || 2525;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
