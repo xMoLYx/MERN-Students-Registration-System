1e7b9cd2f7298b7b7db8b82a2ac3f944095f5b60
Kacper Milnikel <88721178+xMoLYx@users.noreply.github.com>
2024-12-12 23:42:49 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,23 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Database connected'))
 .catch((err) => console.log('Database not connected'))
 
+const corsOptions = {
+  origin: [
+    'http://localhost:4173', // Lokalny adres dla testów
+    'http://localhost:5173', // Lokalny adres dla testów (Vite)
+    'https://mern-students-registration-system-kli3-nyki9h6or.vercel.app', // Frontend na Vercel
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Dozwolone metody
+  credentials: true, // Umożliwia ciasteczka i nagłówki uwierzytelniania
+};
+
 //middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}))
 app.use(cors());
 app.use('/', require('./routes/authRoutes'))
+app.use(cors(corsOptions));
 
 const port = 2525;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
