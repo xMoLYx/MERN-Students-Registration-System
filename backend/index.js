e4e86e951a3d1cab40f788b5f7b40075f4a937b3
Kacper Milnikel <88721178+xMoLYx@users.noreply.github.com>
2024-12-12 22:01:09 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,5 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/authRoutes'))
 
-const port = 8000;
+const port = 2525;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
